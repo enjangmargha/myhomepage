dcd4d101273ca6133b912f7711e3c5c2067d70bd
enjangmargha <enjangsasana@gmail.com>
2025-02-25 19:24:58 +0700

    login

---
+++ app/src/main/java/com/example/myhomepage/LoginActivity.kt
@@ -0,0 +1,37 @@
+package com.example.myhomepage
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.myhomepage.MainActivity
+import com.example.myhomepage.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val editTextUsername = findViewById<EditText>(R.id.editTextUsername)
+        val editTextPassword = findViewById<EditText>(R.id.editTextPassword)
+        val buttonLogin = findViewById<Button>(R.id.buttonLogin)
+
+        buttonLogin.setOnClickListener {
+            val username = editTextUsername.text.toString()
+            val password = editTextPassword.text.toString()
+
+            if (username == "admin" && password == "1234") {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Login Gagal", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
